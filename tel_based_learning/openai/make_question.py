5efd3f0592867245b660422f8a907ada5dea85ff
Gwangui <recordame@kftc.or.kr>
2025-11-27 22:48:27 +0900

    질문 평가를 위한 숙련 등급별 질문작성 요구사항 정의 및 초기 샘플 데이터 생성

--- tel_based_learning/openai/make_question.py
+++ tel_based_learning/openai/make_question.py
@@ -15,17 +15,19 @@ client = OpenAI(
 )
 
 # 아래 전문가 레벨을 조절하여 질문 생성
-# default: 초기 프롬프트
-# tuned: 금융관련 역할을 더 강화시킨 것
-expert_level = "tuned"
+# base: 일반사람
+# low:  초급인력 (퍼플렉시티 기준: 중급 상단~고급 초입)
+# mid:  중급인력 (고급 독해자)
+# high: 고급인력 (최상위 고급, 즉 전문가 수준)
+expert_levels = ["base", "low", "mid", "high"]
+num_of_data = 4
+max_bath = 1
 
-
-def batch_chat_template(batch, prompt, args):
+def batch_chat_template(batch, prompt, expert_level, args):
     messages = []
     system_prompt = prompt["system"][expert_level].format(domain_name=args.domain)
 
     for article in batch["Article"]:
-        print(article)
         user_prompt = prompt["user"].format(domain_name=args.domain, input_text=article)
 
         messages.append(
@@ -39,8 +41,7 @@ def batch_chat_template(batch, prompt, args):
 
 
 def parsing_q_list(row):
-    generated_text = row["generated_text"]
-    qa_list = generated_text.get("questions", [])
+    qa_list = row["questions"]
 
     return {"qa_list": json.dumps(qa_list, ensure_ascii=False, indent=2)}
 
@@ -59,70 +60,70 @@ def main(args):
         print(f"압축 해제 완료.")
 
     print(f"데이터셋 로드 중...")
-    dataset = load_dataset("csv", data_files=csv_path, split="train").select(range(1))
+    dataset = load_dataset("csv", data_files=csv_path, split="train").select(range(num_of_data))
     print(f"데이터셋 로드 완료. 총 {len(dataset)}개 처리 예정.")
 
-    output_filename = f'results_{expert_level}_{args.domain}_{pd.Timestamp.now(tz="Asia/Seoul").strftime("%Y%m%d_%H%M%S")}.json'
-    processed_dataset = dataset
+    for expert_level in expert_levels:
+        output_filename = f'results_{args.domain}_{expert_level}.json' #_{pd.Timestamp.now(tz="Asia/Seoul").strftime("%Y%m%d_%H%M%S")}
+        processed_dataset = dataset
 
-    formatted_msg = []
+        formatted_msg = []
 
-    with open(output_filename, "w", encoding="utf-8") as f:
-        for batch in tqdm(
-            processed_dataset.iter(batch_size=args.max_batch_size),
-            total=ceil(len(processed_dataset) / args.max_batch_size),
-        ):
-            messages_list = batch_chat_template(batch, prompt, args)
+        with open(output_filename, "w", encoding="utf-8") as f:
+            for batch in tqdm(
+                processed_dataset.iter(batch_size=args.max_batch_size),
+                total=ceil(len(processed_dataset) / args.max_batch_size),
+            ):
+                messages_list = batch_chat_template(batch, prompt, expert_level, args)
 
-            for headline, messages in zip(batch["Headline"], messages_list):
-                try:
-                    response = client.chat.completions.create(
-                        model=model_name,
-                        messages=messages,
-                        max_tokens=4096,
-                        temperature=0.1,
-                    )
-
-                    generated_text = response.choices[0].message.content
-
-                    # 답변이 json형식이 아닌 경우, json 응답을 내놓을 때 까지 반복
+                for headline, article, messages in zip(batch["Headline"], batch["Article"], messages_list):
                     try:
-                        json.loads(generated_text)
+                        response = client.chat.completions.create(
+                            model=model_name,
+                            messages=messages,
+                            max_tokens=4096,
+                            temperature=0.7,
+                        )
+
+                        questions = response.choices[0].message.content
+
+                        # 답변이 json형식이 아닌 경우, json 응답을 내놓을 때 까지 반복
+                        try:
+                            json.loads(questions)
+                        except:
+                            continue
+
+                        formatted_msg.append(
+                            {
+                                "headline": headline,
+                                "article": article,
+                                "questions": json.loads(questions),
+                            }
+                        )
                     except:
                         continue
 
-                    formatted_msg.append(
-                        {
-                            "Headline": headline,
-                            "generated_text": json.loads(generated_text),
-                        }
-                    )
-                except Exception as e:
-                    print(f"Error processing '{headline[:30]}...': {e}")
-                    continue
+            json.dump(formatted_msg, f, ensure_ascii=False, indent=2)
 
-        json.dump(formatted_msg, f, ensure_ascii=False, indent=2)
+        # with open(output_filename, "r", encoding="utf-8") as f:
+        #     files = json.load(f)
 
-    with open(output_filename, "r", encoding="utf-8") as f:
-        files = json.load(f)
+        # new_files = []
 
-    new_files = []
+        # for Article, file in zip(dataset["Article"], files):
+        #     file["Article"] = Article
+        #     new_files.append(file)
 
-    for Article, file in zip(dataset["Article"], files):
-        file["Article"] = Article
-        new_files.append(file)
+        # files = new_files
 
-    files = new_files
-    ds_filtered = Dataset.from_list(files)
-
-    ds_parsed = ds_filtered.map(parsing_q_list, num_proc=32)
-    ds_parsed.save_to_disk("temp")
-
+        # ds_filtered = Dataset.from_list(files)   
+        # ds_parsed = ds_filtered.map(parsing_q_list, num_proc=32)
+        # ds_parsed.save_to_disk("temp")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="OpenAI Inference Script")
     parser.add_argument("--domain", type=str, default="finance", help="dataset domain")
-    parser.add_argument("--max_batch_size", type=int, default=1, help="batch_size")
+    parser.add_argument("--max_batch_size", type=int, default=max_bath, help="batch_size")
     parser.add_argument("--lang", type=str, default="korean", help="lang")
     parser.add_argument(
         "--prompt_type", type=str, default="qa_pair_with_re", help="prompt type"
